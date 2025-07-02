70004683319e591143448a0fad6304ce61824ba3
jadson <jadson.rodrigues1996@gmail.com>
2025-07-02 12:46:12 -0300

    Corrige URLs do fetch para usar backend online

--- server.js
+++ server.js
@@ -10,15 +10,12 @@ app.use(cors());
 app.use(express.json());
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
 });
+
 
 module.exports = pool;
 
